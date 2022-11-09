b8fe21c897353c7d8310b49aa6c7f0cbbe64bc7a
RohanD787 <83652628+RohanD787@users.noreply.github.com>
2022-11-09 20:12:58 +0000

    Add files via upload

---
+++ Conformal Preditcion.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[31]:
+
+
+def genData(x1,x2,s,label,n):
+    import numpy as np
+    import pandas as pd
+    np.random.seed()
+    X1 = []
+    X2 = []
+    #X = []
+    Y = [label]*n
+    for i in range(n):
+        X1.append(x1 + s*np.random.normal(0,1))
+        X2.append(x2 + s*np.random.normal(0,1))
+
+        #f1 = x1 + s*np.random.normal(0,1)
+       # f2 = x2 + s*np.random.normal(0,1)
+        #X.append([f1,f2])
+        
+    dt = {'X1':X1, 'X2':X2, 'Y':Y}
+    Data = pd.DataFrame(data=dt)
+    return Data
+
+
+# In[32]:
+
+
+class Node():
+    
+    def __init__(self, X_index=None, threshold=None, left=None, right=None, info_gain=None, label=None, impurity=0,vote=0):
+        
+            self.X_index = X_index
+            self.threshold = threshold
+            self.left = left
+            self.right = right
+            self.info_gain = info_gain
+            self.leaf = False
+            
+
+            self.label = label
+            self.leaf = True
+            self.impurity = impurity
+            self.vote = vote
+
+
+# In[33]:
+
+
+class DecisionTreeClassifier():
+    def __init__(self, min_samples_split=2, max_depth=2,method='gini',leafs={},l=0):
+        
+        # initialize the root of the tree 
+        self.root = None
+        
+        # stopping conditions
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        
+        self.method = method
+        self.leafs = leafs
+        self.l=l
+        
+    def build_tree(self, dataset, curr_depth=0):
+
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        
+        # split until stopping conditions are met
+        if curr_depth<=self.max_depth:
+            # find the best split
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            # check if information gain is positive
+            if best_split["info_gain"]>0:
+                # recur left
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                # recur right
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                # return decision node
+                return Node(best_split["feature_index"], best_split["threshold"], 
+                            left_subtree, right_subtree, best_split["info_gain"])
+        
+        # compute leaf node
+        leaf_value = self.calculate_leaf_value(Y)
+        
+        #storing impurity and vote of leaf
+        v = self.get_vote(leaf_value,Y)
+        if(self.method=='gini'):
+            imp = self.gini_index(Y)
+        else:
+            imp = self.entropy(Y)
+            
+        # return leaf node
+        return Node(label=leaf_value,impurity=imp,vote=v)
+    
+    def get_best_split(self, dataset, n, Xn):
+        
+        
+        # dictionary to store the best split
+        best_split = {}
+        max_info_gain = -float("inf")
+        
+        # loop over all the features
+        for Xi in range(Xn):
+            x = dataset[:, Xi]
+            
+            max_x = np.max(x)
+            min_x = np.min(x)
+            possible_thresholds = np.arange(min_x,max_x,0.2)
+            #possible_thresholds = np.unique(x)
+            # loop over all the feature values present in the data
+            for threshold in possible_thresholds:
+                # get current split
+                dataset_left, dataset_right = self.split(dataset, Xi, threshold)
+                # check if childs are not null
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    # compute information gain
+                    curr_info_gain = self.info_gain(y, left_y, right_y, self.method)
+                    # update the best split if needed
+                    if curr_info_gain>max_info_gain:
+                        best_split["feature_index"] = Xi
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["info_gain"] = curr_info_gain
+                        max_info_gain = curr_info_gain
+                        
+        # return best split
+        return best_split
+    
+    def split(self, dataset, feature_index, threshold):
+        
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
+    
+        
+    def info_gain(self, parent, l_child, r_child, method='gini'):
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        
+        if method=='gini':    #computing info gain with gini index
+            info_gain = self.gini_index(parent) - (weight_l*self.gini_index(l_child) + weight_r*self.gini_index(r_child))
+            
+        elif method=='entropy':                #computing info gain with entropy
+            info_gain = self.entropy(parent) - (weight_l*self.entropy(l_child) + weight_r*self.entropy(r_child))
+            
+        return info_gain
+    
+    def entropy(self, Y):
+        labels = np.unique(Y)
+        entropy = 0
+        for l in labels:
+            p_cls = len(Y[Y == l]) / len(Y)
+            entropy += -p_cls * np.log2(p_cls)
+        return entropy
+    
+    def gini_index(self, Y):
+        labels = np.unique(Y)
+        gini = 0
+        for l in labels:
+            p_cls = len(Y[Y == l]) / len(Y)
+            gini += p_cls**2
+        return 1 - gini
+    
+    def calculate_leaf_value(self, Y):
+        Y = list(Y)
+        return max(Y, key=Y.count)
+    
+    def fit(self, X, Y):
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+        
+    def predict_for_data(self,X):
+        Y = []
+        for x in X:
+            y = self.predict_for_x(x,self.root)     #passing x and root of tree, getting prediction y for x
+            Y.append(y)
+        return Y
+    
+    def predict_for_x(self,X,node):
+        #similar to 'predict_for_sample', but only used on test data without using CP
+        
+        if(node.label!=None):       #if node is leaf node, returning it's class
+            return node.label
+        
+        x = X[node.X_index]
+        
+        if(x<=node.threshold):
+            return self.predict_for_x(X,node.left)    #if x<=threshold, going to left node
+        else:
+            return self.predict_for_x(X,node.right)   #if x>threshold, going to right node
+    
+    def predict_for_sample(self,X,node):
+        #similar to predict_for_x, but used while applying CP
+        
+        if(node.label!=None):       #if node is leaf node, returning it's class
+            return node.label, node.impurity, node
+        
+        x = X[node.X_index]
+        
+        if(x<=node.threshold):
+            return self.predict_for_sample(X,node.left)    #if x<=threshold, going to left node
+        else:
+            return self.predict_for_sample(X,node.right)   #if x>threshold, going to right node
+        
+    def print_tree(self, tree=None, indent=" "):
+        
+        if not tree:
+            tree = self.root
+
+        if tree.label is not None:
+            print(tree.label)
+            print(f'node_num:{tree.num}')
+            print(f'Impurity: {tree.impurity}')
+            print(f'Vote: {tree.vote}')
+
+        else:
+            print("X_"+str(tree.X_index), "<=", tree.threshold, "?", tree.info_gain)
+            print("%sleft:" % (indent), end="")
+            self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
+            self.print_tree(tree.right, indent + indent)
+    
+
+    def get_vote(self,label,Y):
+        c = 0
+        for y in Y:
+            if y==label:
+                c+=1
+            
+        return c/len(Y)
+
+
+# In[34]:
+
+
+import pandas as pd
+from sklearn.utils import shuffle
+import numpy as np
+from sklearn.metrics import accuracy_score
+n=300
+
+#Generating training data
+Train_class1 = genData(2,3,1,1,300)
+Train_class2 = genData(0,0,2,-1,300)
+train_data = pd.concat([Train_class1, Train_class2])
+train_data = train_data.sample(frac=1).reset_index(drop=True)
+X_train = train_data.iloc[:, :-1].values
+Y_train = train_data.iloc[:, -1].values.reshape(-1,1)
+
+#Generating data to test CP
+Test_class1 = genData(2,3,1,1,180)
+Test_class2 = genData(0,0,2,-1,180)
+test_data = pd.concat([Test_class1, Test_class2])
+test_data = test_data.sample(frac=1).reset_index(drop=True)
+X_test = test_data.iloc[:, :-1].values
+Y_test = test_data.iloc[:, -1].values.reshape(-1,1)
+data = X_train
+sample = X_test
+
+model = DecisionTreeClassifier(method='entropy')
+model.fit(X_train,Y_train)
+
+
+# In[35]:
+
+
+def get_conformity_by_vote(data,sample):
+    np.seterr(divide='ignore')
+    conformity_scores = []
+    p_vals = []
+    
+    for x in data:
+        label,impurity,node = model.predict_for_sample(x,model.root)
+        cs = node.vote
+        conformity_scores.append(cs)
+    ranks = sorted(np.unique(conformity_scores))
+    #print(ranks)
+    
+    for i in range(len(sample)):
+        label,impurity,node = model.predict_for_sample(sample[i],model.root)
+        p_val = []
+        if(label==1):
+            vote_1 = node.vote
+            vote_neg1 = 1 - node.vote
+    
+        else:
+            vote_1 = 1 - node.vote
+            vote_neg1 = node.vote
+
+        cs_1 = vote_1
+        cs_neg1 = vote_neg1
+    
+        ranks.append(cs_1)
+        ranks = sorted(ranks)
+        rank_1 = ranks.index(cs_1) + 1
+        #print(rank_1)
+        p_val_1 = (rank_1)/len(ranks)
+        #print('%d / %d = %.2f ' %(rank_1,len(ranks),p_val_1))
+        p_val.append(p_val_1)
+        ranks.remove(cs_1)
+
+        ranks.append(cs_neg1)
+        ranks = sorted(ranks)
+        rank_neg1 = ranks.index(cs_neg1) + 1
+        #print(rank_1)
+        p_val_neg1 = (rank_neg1)/len(ranks)
+        #print('%d / %d = %.2f ' %(rank_neg1,len(ranks),p_val_neg1))
+        p_val.append(p_val_neg1)
+        ranks.remove(cs_neg1)
+        
+        p_vals.append(p_val)
+    #print(conformity_scores)
+    return p_vals
+
+
+# In[36]:
+
+
+def get_conformity_by_vote_randomized(data,sample):
+    np.seterr(divide='ignore')
+    conformity_scores = []
+    p_vals = []
+    
+    for x in data:
+        label,impurity,node = model1.predict_for_sample(x,model.root)
+        cs = node.vote
+        conformity_scores.append(cs)
+    ranks = sorted(np.unique(conformity_scores))
+    
+    for i in range(len(sample)):
+        label,impurity,node = model1.predict_for_sample(sample[i],model.root)
+        p_val = []
+        if(label==1):
+            vote_1 = node.vote
+            vote_neg1 = 1 - node.vote
+    
+        else:
+            vote_1 = 1 - node.vote
+            vote_neg1 = node.vote
+
+        cs_1 = vote_1
+        cs_neg1 = vote_neg1
+        #print(cs_1)
+        p_val_1 = 0
+        for j in reversed(range(len(ranks))):
+            if(cs_1>ranks[j]):
+                if(cs_1==ranks[j+1]):
+                    alpha = 1
+                else:
+                    alpha = 0
+                p_val_1 = ((j+1)+alpha)/len(ranks)
+                break
+      
+        p_val.append(p_val_1)
+
+        p_val_neg1 = 0
+        for j in reversed(range(len(ranks))):
+            if(cs_neg1>ranks[j]):
+                if(cs_neg1==ranks[j+1]):
+                    alpha = 1
+                else:
+                    alpha = 0
+                p_val_neg1 = ((j+1)+alpha)/len(ranks)
+                break
+        
+            
+        p_val.append(p_val_neg1)
+    
+        
+        p_vals.append(p_val)
+    #print(conformity_scores)
+    return p_vals
+
+
+# In[ ]:
+
+
+
+
+
+# In[37]:
+
+
+#CP
+Y = []
+p_val = get_conformity_by_vote(data,sample)
+    
+for i in range(len(p_val)):
+    y = -1 if(p_val[i][0]<p_val[i][1]) else 1
+    Y.append(y)
+    #print('X: %d, %d P_val: %.2f, %.2f Y: %d \n' % (X_test[i][0],X_test[i][1],p_val[i][0],p_val[i][1],y))
+print(accuracy_score(Y_test,Y))
+
+
+# In[38]:
+
+
+#CP with randomized p-values
+Y = []
+p_val = get_conformity_by_vote_randomized(data,sample)
+
+for i in range(len(p_val)):
+    y = -1 if(p_val[i][0]<p_val[i][1]) else 1
+    Y.append(y)
+    #print('X: %d, %d P_val: %.2f, %.2f Y: %d \n' % (X_test[i][0],X_test[i][1],p_val[i][0],p_val[i][1],y))
+print(accuracy_score(Y_test,Y))
+
+
+# In[ ]:
+
+
+
+
